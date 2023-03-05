72401434a1c17257ba6fc07ba637f05a334ddc02
Hiep <hiep@Zipcoders-MacBook-Pro-80.local>
2023-03-05 12:30:58 -0500

    you are not alice or bob

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,32 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+
+            Scanner scan = new Scanner(System.in);
+
+
+            while(true) {
+
+                System.out.print("What is your name: ");
+                String name = scan.next();
+
+                if (name.equals("Alice") || name.equals("Bob"))
+                {
+                    System.out.println("Hello " + name);
+
+                } else {
+                    System.out.println("You are not Alice or Bob!!! Please leave!");
+                }
+
+            }
+
+
+
 
     }
 }
